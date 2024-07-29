3ed6d1d6e566095bc37a846130bc5a4ea028d0e3
Franklin Clark <ffffranklin@users.noreply.github.com>
2024-07-28 18:03:44 -0700

    👷 stub scss files in jest

--- packages/interface/jest.config.ts
+++ packages/interface/jest.config.ts
@@ -1,3 +1,8 @@
 export default {
   preset: "ts-jest",
+  testEnvironment: "node",
+  transform: {
+    '\\.(scss|css|jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/jest/stub-transformer.js',
+  }
 };
