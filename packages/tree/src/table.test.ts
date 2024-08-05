b2496d0f58b0df6bfba736bfad2f6b4746eaf39a
Franklin Clark <ffffranklin@users.noreply.github.com>
2024-08-04 18:02:25 -0700

    ✅ add test file for table module

---
+++ packages/tree/src/table.test.ts
@@ -0,0 +1,9 @@
+import { treeTable } from "./table";
+
+describe('Table', ()=> {
+  describe('treeTable', ()=> {
+    it('should throw if table cant be created', ()=> {
+      expect(treeTable).toThrow('implement me');
+    })
+  });
+});
