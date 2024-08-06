ab92385ae470e64f407394a5db18b532194c8c79
Franklin Clark <ffffranklin@users.noreply.github.com>
2024-08-05 22:48:13 -0700

    ✨ add dom and logic for graph data table

--- packages/interface/src/charts/GraphDataTable/GraphDataTable.tsx
+++ packages/interface/src/charts/GraphDataTable/GraphDataTable.tsx
@@ -2,16 +2,76 @@ import cx from "classnames";
 import React, { FunctionComponent } from "react";
 import { GraphDataTableProps } from "./types";
 import styles from "./GraphDataTable.module.scss";
+import { createColumnHelper, flexRender, getCoreRowModel, useReactTable } from "@tanstack/react-table";
+import { TableDataRow } from "@exp/tree";
 
 
 export const GraphDataTable: FunctionComponent<GraphDataTableProps> = (
   props
 ) => {
-  const { className } = props;
+  const { className, data } = props;
+
+  const columnHelper = createColumnHelper<TableDataRow>()
+
+  const columns = data.schema.columns.map((column) =>
+    columnHelper.accessor(column.field as any, {
+      cell: info => info.getValue()
+    })
+  )
+
+  const table = useReactTable<TableDataRow>({
+    data: data.data,
+    columns,
+    getCoreRowModel: getCoreRowModel()
+  })
 
   return (
     <div className={cx(styles["graph-data-chart"], className)}>
-      Table goes here
+      <table>
+        <thead>
+        {table.getHeaderGroups().map(headerGroup => (
+          <tr key={headerGroup.id}>
+            {headerGroup.headers.map(header => (
+              <th key={header.id}>
+                {header.isPlaceholder
+                  ? null
+                  : flexRender(
+                    header.column.columnDef.header,
+                    header.getContext()
+                  )}
+              </th>
+            ))}
+          </tr>
+        ))}
+        </thead>
+        <tbody>
+        {table.getRowModel().rows.map(row => (
+          <tr key={row.id}>
+            {row.getVisibleCells().map(cell => (
+              <td key={cell.id}>
+                {flexRender(cell.column.columnDef.cell, cell.getContext())}
+              </td>
+            ))}
+          </tr>
+        ))}
+        </tbody>
+        <tfoot>
+        {table.getFooterGroups().map(footerGroup => (
+          <tr key={footerGroup.id}>
+            {footerGroup.headers.map(header => (
+              <th key={header.id}>
+                {header.isPlaceholder
+                  ? null
+                  : flexRender(
+                    header.column.columnDef.footer,
+                    header.getContext()
+                  )}
+              </th>
+            ))}
+          </tr>
+        ))}
+        </tfoot>
+      </table>
     </div>
   )
 }
